e99af39f6b3322ec1434668cbaabef1e61048766
Marks686 <17829791810@163.com>
2022-10-02 23:10:22 +0800

    测试代码

---
+++ test_10.2/test_10.2/test.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("hello bit\n");
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "hello world" << endl;
+//	return 0;
+//}
+
+//#include<iostream>
+//
+//int main()
+//{
+//	std::cout << "hello world" << std::endl;
+//	return 0;
+//}
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+namespace bit
+{
+	int rand = 0;
+}
+int main()
+{
+	printf("hello world\n");
+	printf("%d\n", rand);
+	return 0;
+}
