2dacecc8663ed44165011121513d52187e6ff1f6
Marks686 <17829791810@163.com>
2022-12-13 01:08:56 +0800

    动态内存管理 定位new

---
+++ CPP-20221212/CPP-20221212/Test.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+//class A
+//{
+//public:
+//	A(int a = 0)
+//		: _a(a)
+//	{
+//		cout << "A():" << this << endl;
+// }
+// ~A()
+// {
+//	 cout << "~A():" << this << endl;
+// }
+//private:
+//	int _a;
+//};
+//
+//int main()
+//{
+//	//A* p1 = new A;
+//	////A* p2 = new A[10];
+//	//delete p1;
+//	A* p3 = (A*)malloc(sizeof(A));
+//	if (p3 == nullptr)
+//	{
+//		perror("malloc fail");
+//		exit(-1);
+//	}
+//	//定位new/replacement new --对p3指向空间，显示调用构造函数初始化
+//	new(p3)A(1);
+//	//释放
+//	p3->~A();//析构函数可以显示调用
+//	free(p3);
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
