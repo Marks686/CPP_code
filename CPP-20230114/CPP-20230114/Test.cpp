12c834356a912057c9962cf4f6e89a647f7aa32c
Marks686 <17829791810@163.com>
2023-01-16 20:26:42 +0800

    vector

---
+++ CPP-20230114/CPP-20230114/Test.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include "vector.h"
+#include<assert.h>
+#include<vector>
+using namespace std;
+
+
+int main()
+{
+	bit::test_vector1();
+	return 0;
+}
