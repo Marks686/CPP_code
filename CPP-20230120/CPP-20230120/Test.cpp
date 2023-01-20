a4083c880959d76f96a69e0aab189467a3c00e6d
Marks686 <17829791810@163.com>
2023-01-20 20:02:38 +0800

    测试代码

---
+++ CPP-20230120/CPP-20230120/Test.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 1;
+	int b;
+	scanf("%d", &b);
+	printf("%d ", a);
+	printf("%d", b);
+
+	return 0;
+}
